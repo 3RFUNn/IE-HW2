3289ebb9df5a2b7086ebbbeb473d789181ee8ff5
3RFUNn <efnoskuee@gmail.com>
2023-05-12 16:26:32 +0330

    commit

--- actions/auth.js
+++ actions/auth.js
@@ -2,3 +2,23 @@ import {UserModel} from "../models/index.js";
 import { generatePasswordHash } from "../utils/hash.js";
 import {generateAccessToken} from "../utils/jwt-helper.js";
 import {validateEmail, validatePassword} from "../utils/validators.js";
+
+export const loginUser = async (email = "", password = "") => {
+    try {
+        console.log("login =>", email, password);
+        if ((id?.length > 0) && validatePassword(password)) {
+            const user = await UserModel.findOne({email});
+            if (await comparePasswordHash(user.password, password)) {
+                const token = generateAccessToken({
+                    id: user.id,
+                    email: user.email,
+                    userType: user.userType,
+                });
+                return token;
+            }
+        }
+        return null;
+    } catch (error) {
+        return null;
+    }
+};
