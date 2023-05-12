86f6deb41aab03f08c53b9bcdc36af6aa8bf141d
3RFUNn <efnoskuee@gmail.com>
2023-05-12 17:37:27 +0330

    export
    get courses handler

--- controllers/courses.js
+++ controllers/courses.js
@@ -100,3 +100,83 @@ export const getCourseHandler = async (req, res) => {
             });
     }
 };
+
+export const getCoursesHandler = async (req, res) => {
+    try {
+        const courses = await getCourses();
+        const user = await getUser(req?.authData.id);
+        if (user?.userType === "STUDENT") {
+            res.status(200)
+                .json({
+                    data: courses.filter((c) => user.courses.includes(c.id)),
+                    status: 200,
+                    error: null,
+                    ok: true,
+                    message: "",
+                })
+                .end(() => {
+                    // log
+                });
+        } else if (user?.userType === "PROFESSOR") {
+            res.status(200)
+                .json({
+                    data: courses.filter((c) => c.professorId === user.id),
+                    status: 200,
+                    error: null,
+                    ok: true,
+                    message: "",
+                })
+                .end(() => {
+                    // log
+                });
+        } else if (user?.userType === "EDU_MANAGER") {
+            res.status(200)
+                .json({
+                    data: courses,
+                    status: 200,
+                    error: null,
+                    ok: true,
+                    message: "",
+                })
+                .end(() => {
+                    // log
+                });
+        } else if (user.userType === "IT_MANAGER") {
+            res.status(200)
+                .json({
+                    data: courses,
+                    status: 200,
+                    error: null,
+                    ok: true,
+                    message: "",
+                })
+                .end(() => {
+                    // log
+                });
+        } else {
+            res.status(400)
+                .json({
+                    data: null,
+                    status: 400,
+                    error: "access denied",
+                    ok: false,
+                    message: "you dont have permission",
+                })
+                .end(() => {
+                    // log
+                });
+        }
+    } catch (error) {
+        res.status(400)
+            .json({
+                data: null,
+                status: 400,
+                error: error,
+                ok: false,
+                message: "error in get course",
+            })
+            .end(() => {
+                // log
+            });
+    }
+};
