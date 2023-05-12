a3b6fe848245cb3b6d18a27085c44933d398700c
3RFUNn <efnoskuee@gmail.com>
2023-05-12 18:29:02 +0330

    handler
    student and students

--- controllers/users.js
+++ controllers/users.js
@@ -20,3 +20,96 @@ import {
     getStudent,
     getStudents,
 } from "../actions/users.js";
+
+export const getStudentsHandler = async (req, res) => {
+    try {
+        if (
+            req?.authData?.userType === "IT_MANAGER" ||
+            req.authData?.userType === "EDU_MANAGER"
+        ) {
+            const students = await getStudents();
+            res.status(200)
+                .json({
+                    data: students,
+                    status: 200,
+                    error: null,
+                    ok: true,
+                    message: "",
+                })
+                .end(() => {
+                    // log
+                });
+        } else {
+            res.status(400)
+                .json({
+                    data: null,
+                    status: 400,
+                    error: "access denied",
+                    ok: false,
+                    message: "you dont have permission",
+                })
+                .end(() => {
+                    // log
+                });
+        }
+    } catch (error) {
+        res.status(400)
+            .json({
+                data: null,
+                status: 400,
+                error: error,
+                ok: false,
+                message: "error in get students",
+            })
+            .end(() => {
+                // log
+            });
+    }
+};
+
+export const getStudentHandler = async (req, res) => {
+    try {
+        if (
+            req?.authData?.id === req?.params?.id ||
+            req?.authData?.userType === "IT_MANAGER" ||
+            req.authData?.userType === "EDU_MANAGER"
+        ) {
+            const student = await getStudent(req?.params?.id);
+            res.status(200)
+                .json({
+                    data: student,
+                    status: 200,
+                    error: null,
+                    ok: true,
+                    message: "",
+                })
+                .end(() => {
+                    // log
+                });
+        } else {
+            res.status(400)
+                .json({
+                    data: null,
+                    status: 400,
+                    error: "access denied",
+                    ok: false,
+                    message: "you dont have permission",
+                })
+                .end(() => {
+                    // log
+                });
+        }
+    } catch (error) {
+        res.status(400)
+            .json({
+                data: null,
+                status: 400,
+                error: error,
+                ok: false,
+                message: "error in get student",
+            })
+            .end(() => {
+                // log
+            });
+    }
+};
