2f3c75ba1a223ab673c5ca62d280e09da882b512
3RFUNn <efnoskuee@gmail.com>
2023-05-12 19:08:43 +0330

    imports

---
+++ main.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import morgan from "morgan";
+import {config} from "dotenv";
+import {usersRouter} from "./routes/users.js";
+import {authRouter} from "./routes/auth.js";
+import {courseRouter} from "./routes/courses.js";
+import swaggerUI from "swagger-ui-express";
+// import swaggerJsDoc from "swagger-jsdoc";
+import swaggerDoc from "./swagger.json" assert {type: "json"};
