8d67a959071dbe65a50b51ebfeb33a98f82f9355
3RFUNn <efnoskuee@gmail.com>
2023-05-12 17:36:18 +0330

    new imports

--- controllers/courses.js
+++ controllers/courses.js
@@ -5,3 +5,11 @@ import {
     getCourses,
     editCourse,
 } from "../actions/courses.js";
+
+import {
+    getStudent,
+    getStudents,
+    getUser,
+    editStudent,
+    getProfessor,
+} from "../actions/users.js";
