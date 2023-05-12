19604d77aa3ebb5491aed808294306b28c5a3bc3
3RFUNn <efnoskuee@gmail.com>
2023-05-12 18:52:08 +0330

    course schema

--- models/index.js
+++ models/index.js
@@ -60,3 +60,59 @@ const userSchema = new Schema({
     },
 });
 
+//Course Schema creation
+
+const courseSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    prerequisite: {
+        type: [String],
+        required: true,
+    },
+    coPrerequisite: {
+        type: [String],
+        required: true,
+    },
+    unit: {
+        type: Number,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ["APPROVED", "TERM"],
+        default: "APPROVED",
+        required: true,
+    },
+    classTime: {
+        type: String,
+    },
+    examTime: {
+        type: String,
+    },
+    examLocation: {
+        type: String,
+    },
+    professorName: {
+        type: String,
+    },
+    professorId: {
+        type: String,
+        required: true,
+    },
+    capacity: {
+        type: Number,
+    },
+    term: {
+        type: String,
+    },
+    students: {
+        type: [String],
+        default: [],
+    },
+});
