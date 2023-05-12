249cfa82716da54525d245b21fe9d7498f9c15ac
3RFUNn <efnoskuee@gmail.com>
2023-05-12 18:51:26 +0330

    user schema

--- models/index.js
+++ models/index.js
@@ -1,1 +1,62 @@
 import {mongoose_client} from "../utils/mongodb-atlas/mongoose.js";
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    id: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    mobileNumber: {
+        type: String,
+        required: true,
+    },
+    userType: {
+        type: String,
+        enum: ["STUDENT", "PROFESSOR", "IT_MANAGER", "EDU_MANAGER"],
+        required: true,
+    },
+    level: {
+        type: String,
+    },
+    entryYear: {
+        type: Number,
+    },
+    entryTerm: {
+        type: String,
+    },
+    gpa: {
+        type: Number,
+        required: function () {
+            return this.userType === "STUDENT";
+        },
+    },
+    department: {
+        type: String,
+    },
+    subject: {
+        type: String,
+    },
+    rank: {
+        type: Number,
+    },
+    courses: {
+        type: [String],
+        default: [],
+    },
+});
+
