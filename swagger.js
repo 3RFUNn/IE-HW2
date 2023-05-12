6b7a5e47c2f2e8aa4ae5f44da83a4b89e7e772a0
3RFUNn <efnoskuee@gmail.com>
2023-05-12 19:09:50 +0330

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,1 +1,11 @@
 import swaggerAutogen from 'swagger-autogen';
+
+const outputFile = "./swagger_output.json";
+
+const endpointsFiles = [
+    "./routes/users.js",
+    "./routes/auth.js",
+    "./routes/courses.js",
+];
+
+swaggerAutogen()(outputFile, endpointsFiles);
