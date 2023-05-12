3a9922251b650cbeeec33ed386a1fbc42e860bde
3RFUNn <efnoskuee@gmail.com>
2023-05-12 19:02:44 +0330

    Create mongoose.js

---
+++ utils/mongodb-atlas/mongoose.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const database_url =
+    process.env.MONGO_URI ||
+    "mongodb+srv://admin:Et2Ozs6CnwZ4CFiR@cluster0.hgphhqj.mongodb.net/?retryWrites=true&w=majority";
+
+
+mongoose.connect(database_url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    dbName:"lms"
+});
+
+export const mongoose_client = mongoose;
