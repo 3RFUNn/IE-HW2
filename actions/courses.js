05cb277730147c95207dfa55cd9e3a4c3ac0b8d6
3RFUNn <efnoskuee@gmail.com>
2023-05-12 17:05:41 +0330

    Update courses.js

--- actions/courses.js
+++ actions/courses.js
@@ -1,1 +1,15 @@
 import {CourseModel} from "../models/index.js";
+
+export const addCourse = async (courseData) => {
+    try {
+        if (courseData?.name?.length > 0) {
+            const course = new CourseModel(courseData);
+            await course.save();
+            return course;
+        }
+        return null;
+    } catch (error) {
+        return null;
+    }
+};
+
