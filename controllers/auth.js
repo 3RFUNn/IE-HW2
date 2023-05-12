ca63176fbc6fbf0d091ef2fd65e7ee3dea02ba4b
3RFUNn <efnoskuee@gmail.com>
2023-05-12 17:33:04 +0330

    login handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,3 +40,45 @@ export const registerUserHandler = async (req, res) => {
             .end(() => {});
     }
 };
+
+export const loginUserHandler = async (req, res) => {
+    try {
+        const token = await loginUser(req?.body?.id, req?.body?.password);
+        if (token) {
+            res.setHeader("Authorization", `Bearer ${token}`)
+                .status(200)
+                .json({
+                    data: token,
+                    status: 200,
+                    error: null,
+                    ok: true,
+                    message: "",
+                })
+                .end(() => {});
+            return;
+        } else {
+            res.status(400)
+                .json({
+                    data: token,
+                    status: 400,
+                    error: "invalid data",
+                    ok: true,
+                    message: "error in login",
+                })
+                .end(() => {});
+            return;
+        }
+    } catch (error) {
+        console.log("error => ", error);
+        res.status(400)
+            .json({
+                data: null,
+                status: 400,
+                error: error,
+                ok: true,
+                message: "error in login",
+            })
+            .end(() => {});
+        return;
+    }
+};
