dae5b7a8a4c93984e398ef5c60e5637af6d2edcb
3RFUNn <efnoskuee@gmail.com>
2023-05-12 17:09:08 +0330

    add func

--- actions/users.js
+++ actions/users.js
@@ -1,1 +1,18 @@
 import {UserModel} from "../models/index.js";
+
+
+export const addProfessor = async (professorData) => {
+    try {
+        if (professorData?.firstName?.length > 0) {
+            const professor = new UserModel({
+                ...professorData,
+                userType: "PROFESSOR",
+            });
+            await professor.save();
+            return professor;
+        }
+        return false;
+    } catch (error) {
+        return false;
+    }
+};
